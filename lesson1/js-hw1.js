4df11f49955c5881c5d42d26d17a66c6a83cdb8c
Yelyzaveta Smetana <yelyzaveta.smetana@hneu.net>
2023-11-20 21:26:11 +0200

    add all

--- lesson1/js-hw1.js
+++ lesson1/js-hw1.js
@@ -1,3 +1,5 @@
+ // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+ // Вивести кожну змінну за допомогою: console.log
  let hello='hello';
  console.log(hello);
  let owu='owu';
@@ -26,11 +28,14 @@
  console.log(f);
 
  console.log("----------------------")
+ // Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенац
  let firstName='Yelyzaveta ';
  let middleName='Yuryevna ';
  let lastName='Smetana';
  console.log(firstName+middleName+lastName)
  console.log("----------------------")
+ // За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+ //   let a = 100; let b = '100'; let c = true;
  let a = 100;
  console.log(typeof a)
  let b = '100';
@@ -38,6 +43,8 @@
  let c = true;
  console.log(typeof c)
  console.log("----------------------")
+ // Додаткове для тих хто цікавився prompt`oм
+ // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
  console.log(prompt(firstName))
  console.log(prompt(middleName))
  console.log(prompt(lastName))
